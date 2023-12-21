e40c5b71cef7b882a581e495464f2fbb4c26c2de
Mohamed Tijani Daoud <mohamed.daoud99@hotmail.com>
2023-12-21 16:43:31 +0100

    commit2

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 require('dotenv').config()
 
 const express = require('express')
-const cors = require('cors')
+//const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const path = require('path')
-const corsOptions = require('./config/cors')
+//const corsOptions = require('./config/cors')
 //const connectDB = require('./config/database')
 const credentials = require('./middleware/credentials')
 const errorHandlerMiddleware = require('./middleware/error_handler')
 const authenticationMiddleware = require('./middleware/authentication')
 
 const app = express()
-const PORT = 3500
 
 //connectDB()
 
@@ -20,7 +19,7 @@ const PORT = 3500
 app.use(credentials)
 
 // CORS
-app.use(cors(corsOptions))
+//app.use(cors(corsOptions))
 
 // application.x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
@@ -40,6 +39,9 @@ app.use('/static', express.static(path.join(__dirname, 'public')))
 // Default error handler
 app.use(errorHandlerMiddleware)
 
+app.get('/', (req, res) => {
+  res.send('Hello NODE API')
+})
 
 // Routes
 app.use('/api/auth', require('./routes/api/auth'))
