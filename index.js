dcf04e0dc6cb0e0035b0abdb1bf439cf9d61bd89
Mohamed Tijani Daoud <mohamed.daoud99@hotmail.com>
2023-12-21 22:47:23 +0100

    ccc

--- index.js
+++ index.js
@@ -1,30 +1,39 @@
 require('dotenv').config()
 
 const express = require('express')
-const cors = require('cors')
+const cors = require('cors') // Include the cors middleware
 const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
 const path = require('path')
-const corsOptions = require('./config/cors')
-//const connectDB = require('./config/database')
-const credentials = require('./middleware/credentials')
+// const corsOptions = require('./config/cors') // Remove this line
+// const connectDB = require('./config/database')
 const errorHandlerMiddleware = require('./middleware/error_handler')
 const authenticationMiddleware = require('./middleware/authentication')
 
 const app = express()
 const PORT = 3500
 
-//connectDB()
+// Connect to MongoDB
+mongoose.set("strictQuery", false)
+mongoose.connect('mongodb+srv://mohameddaoud99:yNDLAWlTL9vtNNag@cluster0.jaaqxok.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(3000, () => {
+      console.log(`Node API app is running on port 3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
-// Allow Credentials
-app.use(credentials)
+// Allow Credentials - Remove this line
+// app.use(credentials)
 
 // CORS
-app.use(cors(corsOptions))
+app.use(cors());
 
 // application.x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false }))
-
 
 // application/json response
 app.use(express.json())
@@ -41,10 +50,8 @@ app.get('/', (req, res) => {
   res.send('Hello NODE API')
 })
 
-
 // Default error handler
 app.use(errorHandlerMiddleware)
-
 
 // Routes
 app.use('/api/auth', require('./routes/api/auth'))
@@ -55,34 +62,7 @@ app.all('*', (req, res) => {
 
   if(req.accepts('json')){
     res.json({'error': '404 Not Found'})
-  }else{
+  } else {
     res.type('text').send('404 Not Found')
   }
-})
-
-app= async (req, res) => {
-  // Your API logic here
-
-  // Set CORS headers
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-
-  // Your response
-  res.status(200).json({ message: 'Success' });
-};
-
-mongoose.set("strictQuery", false)
-mongoose.
-connect('mongodb+srv://mohameddaoud99:yNDLAWlTL9vtNNag@cluster0.jaaqxok.mongodb.net/?retryWrites=true&w=majority')
-.then(() => {
-    console.log('connected to MongoDB')
-    app.listen(3000, ()=> {
-        console.log(`Node API app is running on port 3000`)
-    });
-}).catch((error) => {
-    console.log(error)
-})
-
-module.exports = app;
+});
